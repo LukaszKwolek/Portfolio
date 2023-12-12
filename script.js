9799211eadc12b4b085ace830980231c9a4db7ec
StigerPL <85870161+StigerPL@users.noreply.github.com>
2023-12-12 18:28:14 +0100

    Revert "Update script.js"
    This reverts commit 965d7e7f92445f21cfe8fe4976892e562fa511cd.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
 
     function showCard(index) {
 <<<<<<< HEAD
-<<<<<<< HEAD
         $(".portfolio-card").removeClass("show-card slide-from-bottom slide-from-right slide-from-top slide-from-left");
 
         if (index < currentIndex) {
@@ -16,10 +15,6 @@ $(document).ready(function () {
         }
 
         currentIndex = index;
-=======
-        $(".portfolio-card").hide();
-        $(".portfolio-card:eq(" + index + ")").show();
->>>>>>> parent of 97736ed (Update script.js)
 =======
         $(".portfolio-card").hide();
         $(".portfolio-card:eq(" + index + ")").show();
